3eb5ccc29fb6ab3ccf6fbbbb527f1a287bd9feb9
Tommasso Borgato <tborgato@tborgato.remote.csb>
2018-02-01 14:53:41 +0100

    refactoring servlet

--- src/main/java/sample/HelloWorldServlet.java
+++ src/main/java/sample/HelloWorldServlet.java
@@ -9,21 +9,22 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
+ * Servlet that stores attribute SESSION_ATTRIBUTE_KEY into HTTP session; HTTP session is replicated across wildfly cluster nodes;
  * @author tborgato
  * @since 18.04.17
  */
 @WebServlet(name = "HelloWorldServlet", urlPatterns = { "/helloworld" })
 public class HelloWorldServlet extends HttpServlet {
 
-    private static final String KEY_SERIAL = HelloWorldServlet.class + "-KEY_SERIAL";
+    private static final String SESSION_ATTRIBUTE_KEY = HelloWorldServlet.class + "-SESSION_ATTRIBUTE_KEY";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession(true);
-        Integer serial = (Integer) session.getAttribute(KEY_SERIAL);
+        Integer serial = (Integer) session.getAttribute(SESSION_ATTRIBUTE_KEY);
         if (serial==null){serial=0;}
-        resp.getWriter().append("Tutto a posto a ferragosto [" + serial + "]").flush();
-        session.setAttribute(KEY_SERIAL, serial + 1);
+        resp.getWriter().append("Counter: " + serial + "").flush();
+        session.setAttribute(SESSION_ATTRIBUTE_KEY, serial + 1);
         resp.setHeader("Content-Type", "plain/text");
     }
 }
